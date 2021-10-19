7d16c845194a4ecb44941583fb9bf5580f234719
Bami <bbamii@outlook.com>
2021-10-19 22:32:58 +0100

    updoot 2

--- src/Callback.jsx
+++ src/Callback.jsx
@@ -29,7 +29,7 @@ function Callback({ location }) {
     axios.get('https://ayo-api.herokuapp.com/linkedin-access-token', {
       params: {
         headers: { 'Content-Type:': 'x-www-form-urlencoded' },
-        url: `${REACT_APP_LINKEDIN_ACCESSTOKEN_URL}?grant_type=authorization_code&code=AQT_b-0EbYS_P1YfMPJ8XXmgR3l2YeERzUnTTMf7btW1PYAFcWwlzTG9wqQtcw09n2sksNOQw4mB2997it0OQ_ZZ_PUhYJwNmhegXthyj-ThF-v2hYklkUXPasZqire-ZPOYzlPWG3g_qgDxS-ASXxmJ618BRBQ-DZ4hmrvGh_1MSskIQ8AAkN5aHgzwGyv8FM0yoGJFN_13o9ayqFU&client_id=${REACT_APP_CLIENT_ID}&client_secret=${REACT_APP_CLIENT_SECRET}&redirect_uri=${REACT_APP_REDIRECT_URI}`
+        url: `${REACT_APP_LINKEDIN_ACCESSTOKEN_URL}?grant_type=authorization_code&code=${code}&client_id=${REACT_APP_CLIENT_ID}&client_secret=${REACT_APP_CLIENT_SECRET}&redirect_uri=${REACT_APP_REDIRECT_URI}`
       }
     })
     .then(res => {
